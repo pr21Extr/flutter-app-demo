5c7209f9e9be2f20ad6f1ac2f82da958f6d6760f
CORP\pbhat <pbhat@extremenetworks.com>
2023-07-24 12:45:01 +0530

    added expanded needs review

--- lib/taskList.dart
+++ lib/taskList.dart
@@ -4,13 +4,23 @@ import 'taskItem.dart';
 class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Expanded(
+      child:Column(
       children: [
         TaskItem(label: "Task 1"),
         TaskItem(label: "Task 2"),
         TaskItem(label: "Task 3"),
         TaskItem(label: "Task 4"),
+        Expanded(
+          child: Container(
+            // color: Colors.amber, used Expanded but I dont think its the correct implementation
+            width: 10,
+
+          ),
+
+        )
       ],
+      ),
     );
   }
 }
