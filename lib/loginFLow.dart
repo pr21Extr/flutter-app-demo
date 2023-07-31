960694cbc4d75e92e453d31d6277a95473d61c36
CORP\pbhat <pbhat@extremenetworks.com>
2023-07-31 15:38:29 +0530

    theme and styles

--- lib/loginFLow.dart
+++ lib/loginFLow.dart
@@ -1,41 +1,58 @@
-  import 'package:flutter/material.dart';
-  import 'package:http/http.dart' as http;
-  import 'dart:convert';
-  import 'todoListScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'todoListScreen.dart';
 
-  class LoginFlow extends StatefulWidget {
-    @override
-    _LoginFlowState createState() => _LoginFlowState();
-  }
+class LoginFlow extends StatefulWidget {
+  @override
+  _LoginFlowState createState() => _LoginFlowState();
+}
 
-  class _LoginFlowState extends State<LoginFlow> {
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    String? _username;
-    String? _password;
-    bool _obscureText = true;
+class _LoginFlowState extends State<LoginFlow> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? _username;
+  String? _password;
+  bool _obscureText = true;
 
-    void navigateToTodos(BuildContext context) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => TodosScreen()),
-      );
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Login Page"),
-          centerTitle: true,
-        ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+        color: Colors.white60,
+        padding: const EdgeInsets.all(20.0),
+    child: Align(
+    alignment: Alignment.topLeft, // Align the column to the top left
+    child: Padding(
+    padding: const EdgeInsets.only(top: 50.0), // Adjust top padding to raise the login elements higher
+    child: Form(
+    key: _formKey,
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    // Logo or App Icon (Optional)
+    // Image.asset(
+    //   'assets/app_logo.png',
+    //   height: 150,
+    //   width: 150,
+    // ),
+    Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+    Text("Login",
+    style: TextStyle(
+      fontSize: 30
+    ),
+    ),
+    ],
+    ),
+    SizedBox(height: 25),
+    SizedBox(height: 100),
               TextFormField(
-
-                decoration: InputDecoration(labelText: 'Username'),
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter your username';
@@ -51,6 +68,8 @@
                 obscureText: _obscureText,
                 decoration: InputDecoration(
                   labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
                   suffixIcon: IconButton(
                     onPressed: () {
                       setState(() {
@@ -58,7 +77,9 @@
                       });
                     },
                     icon: Icon(
-                      _obscureText ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+                      _obscureText
+                          ? Icons.visibility
+                          : Icons.visibility_off,
                     ),
                   ),
                 ),
@@ -72,53 +93,71 @@
                   _password = value;
                 },
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    print('check if im called');
-                    _performLogin();    //login function
-                  }
-                },
-                child: Text('Login'),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end, // Align the button to the right
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        _performLogin();
+                      }
+                    },
+                    child: Text('Login'),
+                    style: ElevatedButton.styleFrom(
+                      // Set the button's width to 120 (adjust as needed)
+                      minimumSize: Size(120, 40),
+                      primary:Colors.deepPurpleAccent
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
+      ),
+    ),
+        ),
+    );
+  }
+
+
+  void _performLogin() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+    };
+
+    Map<String, dynamic> requestBody = {
+      'username': _username,
+      'password': _password,
+    };
+
+    try {
+      var response = await http.post(
+        url,
+        headers: headers,
+        body: json.encode(requestBody),
       );
-    }
 
-    void _performLogin() async {
-      var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-
-      Map<String, String> headers = {
-        'Content-Type': 'application/json',
-      };
-
-      Map<String, dynamic> requestBody = {
-        'username': _username,
-        'password': _password,
-      };
-
-      try {
-        var response = await http.post(
-          url,
-          headers: headers,
-          body: json.encode(requestBody),
-        );
-
-        if (response.statusCode >= 200 && response.statusCode < 300) {
-          Map<String, dynamic> responseData = json.decode(response.body);
-          print('Login successful! Response: $responseData');
-          navigateToTodos(context);
-
-
-        } else {
-          print('Login failed. Status Code: ${response.statusCode}');
-        }
-      } catch (e) {
-        print('Error occurred: $e');
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        Map<String, dynamic> responseData = json.decode(response.body);
+        print('Login successful! Response: $responseData');
+        _navigateToTodosScreen(); // Navigate to TodosScreen after successful login
+      } else {
+        print('Login failed. Status Code: ${response.statusCode}');
       }
+    } catch (e) {
+      print('Error occurred: $e');
     }
   }
+
+  void _navigateToTodosScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TodosScreen()),
+    );
+  }
+}
