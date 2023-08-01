f67211a30df3317f35a0733343461ada29f499d0
CORP\pbhat <pbhat@extremenetworks.com>
2023-08-01 13:00:51 +0530

    WIP image refuses to render, rest of Page works

--- lib/loginFLow.dart
+++ lib/loginFLow.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'todoListScreen.dart';
-
 class LoginFlow extends StatefulWidget {
   @override
   _LoginFlowState createState() => _LoginFlowState();
@@ -13,119 +12,132 @@ class _LoginFlowState extends State<LoginFlow> {
   String? _username;
   String? _password;
   bool _obscureText = true;
+  bool _rememberMe = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white60,
+        color: Colors.white,
         padding: const EdgeInsets.all(20.0),
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 50.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all( 20.0),
+                child:
+
+                Text(
+                  "Login",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+                ),
+              ),
+              Image.asset(
+                'assets/header.jpg',
+                height: 150,
+                width: 150,
+              ),
+              SizedBox(height:70),
+              Card(
+                elevation: 3,
+                color: Colors.white60,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
                     children: [
-                      Text(
-                        "Login",
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 25),
-                  Card(
-                    elevation: 3,
-                    color: Colors.grey[100],
-
-
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Username',
-                              border:UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.cyanAccent)
-                              ),
-
-                              prefixIcon: Icon(Icons.person),
-                              labelStyle: TextStyle(color:Colors.deepPurpleAccent)
-
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter your username';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) {
-                              _username = value;
-                            },
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Username',
+                           prefixIcon: Icon(Icons.person),
+                          labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.cyanAccent),
                           ),
-                          SizedBox(height: 30),
-                          TextFormField(
-                            obscureText: _obscureText,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                                labelStyle: TextStyle(color:Colors.deepPurpleAccent),
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.lock),
-                              suffixIcon: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _obscureText = !_obscureText;
-                                  });
-                                },
-                                icon: Icon(
-                                  _obscureText
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
-                              ),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter your password';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) {
-                              _password = value;
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            _formKey.currentState!.save();
-                            _performLogin();
-                          }
-                        },
-                        child: Text('Login'),
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(100, 30),
-                          primary: Colors.deepPurpleAccent,
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your username';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _username = value;
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        obscureText: _obscureText,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                            icon: Icon(
+                              _obscureText ? Icons.visibility : Icons.visibility_off,
+                            ),
+                          ),
+                          labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.cyanAccent),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _password = value;
+                        },
                       ),
                     ],
                   ),
+                ),
+              ),
+              SizedBox(height: 36),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Remember me', style: TextStyle(color: Colors.purpleAccent)),
+                  Radio<bool>(
+                    value: true,
+                    groupValue: _rememberMe,
+                    onChanged: (value) {
+                      setState(() {
+                        _rememberMe = value ?? false;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                  ),
+                  SizedBox(width: 16,height: 40),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        _performLogin();
+                      }
+                    },
+                    child: Text('Login'),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(100, 30),
+                      primary: Colors.deepPurpleAccent,
+                    ),
+                    
+                  ),
+
+
                 ],
               ),
-            ),
+            ],
           ),
         ),
       ),
@@ -170,4 +182,3 @@ class _LoginFlowState extends State<LoginFlow> {
     );
   }
 }
-
